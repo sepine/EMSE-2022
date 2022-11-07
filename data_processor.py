f2552a5fe12140b137d3dea4cebac39610e4cd7b
Stylite <azks.liang@qq.com>
2022-11-07 21:08:42 +0800

    update

--- data_processor.py
+++ data_processor.py
@@ -19,6 +19,7 @@ class DataProcessor(object):
         self.train = train
         self.valid = valid
         self.test = test
+        self.scalar = StandardScaler(copy=True, with_mean=True, with_std=True)
 
     @staticmethod
     def split_feature_label(train):
@@ -27,18 +28,20 @@ class DataProcessor(object):
         x_train = train[:, : -1]
         return x_train, y_train
 
-    @staticmethod
-    def z_score(x):
-        scalar = StandardScaler(copy=True, with_mean=True, with_std=True)
-        x_scaled = scalar.fit_transform(x)
+    def z_score_fit(self, x):
+        x_scaled = self.scalar.fit_transform(x)
+        return x_scaled
+
+    def z_score_transform(self, x):
+        x_scaled = self.scalar.transform(x)
         return x_scaled
 
     def process(self, is_validate=False):
         x_train, y_train = self.split_feature_label(self.train)
         x_test, y_test = self.split_feature_label(self.test)
 
-        x_train_scaled = self.z_score(x_train)
-        x_test_scaled = self.z_score(x_test)
+        x_train_scaled = self.z_score_fit(x_train)
+        x_test_scaled = self.z_score_transform(x_test)
 
         y_train = np.array(y_train, dtype=np.int)
         y_test = np.array(y_test, dtype=np.int)
@@ -47,7 +50,7 @@ class DataProcessor(object):
             return x_train, x_test, x_train_scaled, x_test_scaled, y_train, y_test
 
         x_valid, y_valid = self.split_feature_label(self.valid)
-        x_valid_scaled = self.z_score(x_valid)
+        x_valid_scaled = self.z_score_transform(x_valid)
 
         return x_train, x_valid, x_test, \
             x_train_scaled, x_valid_scaled, x_test_scaled, \
